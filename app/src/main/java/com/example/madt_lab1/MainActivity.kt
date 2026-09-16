41e6bf4fe5bbb0cb1a42bf97076fc633256b07dc
YGetch <vladolefir109@gmail.com>
2026-09-16 16:43:15 +0300

    Add first button to change text

--- app/src/main/java/com/example/madt_lab1/MainActivity.kt
+++ app/src/main/java/com/example/madt_lab1/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.madt_lab1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvMainText = findViewById<TextView>(R.id.tvMainText)
+        val btnChangeText = findViewById<Button>(R.id.btnChangeText)
+
+        btnChangeText.setOnClickListener {
+            tvMainText.text = "Text Changed!"
         }
     }
 }
